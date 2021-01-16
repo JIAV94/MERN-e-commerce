8845963a8f771071e7ebca3f3d1dd599b7674ccc
jiav <yabiz94@gmail.com>
2021-01-16 09:31:57 -0600

    User register and User update

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,14 +6,21 @@ import {
   productDetailsReducer
 } from './reducers/productReducers.js'
 import { cartReducer } from './reducers/cartReducers.js'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers.js'
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer
+} from './reducers/userReducers.js'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
-  userRegister: userRegisterReducer
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') 
