b73a56a04881e07bf0181e2c2550f6fa9e2917f3
jiav <yabiz94@gmail.com>
2021-01-28 06:53:42 -0600

    checkout process

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -8,8 +8,12 @@ import {
   ORDER_DETAILS_FAIL,
   ORDER_PAY_REQUEST,
   ORDER_PAY_SUCCESS,
-  ORDER_PAY_FAIL
+  ORDER_PAY_FAIL,
+  ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_MY_FAIL
 } from "../constants/orderConstants.js"
+import { logout } from './userActions'
 
 export const createOrder = (order) => async (dispatch, getState) => {
   try {
@@ -80,7 +84,10 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
   }
 }
 
-export const payOrder = (orderId, paymentResult) => async (dispatch, getState) => {
+export const payOrder = (orderId, paymentResult) => async (
+  dispatch, 
+  getState
+) => {
   try {
     dispatch({
       type: ORDER_PAY_REQUEST
@@ -97,7 +104,11 @@ export const payOrder = (orderId, paymentResult) => async (dispatch, getState) =
       }
     }
 
-    const { data } = await axios.put(`/api/orders/${orderId}/pay`, paymentResult, config)
+    const { data } = await axios.put(
+      `/api/orders/${orderId}/pay`,
+      paymentResult, 
+      config
+    )
 
     dispatch({
       type: ORDER_PAY_SUCCESS,
@@ -111,6 +122,43 @@ export const payOrder = (orderId, paymentResult) => async (dispatch, getState) =
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message
+    })
+  }
+}
+
+export const listMyOrders = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_LIST_MY_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.get(`/api/orders/myorders`, config)
+
+    dispatch({
+      type: ORDER_LIST_MY_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout())
+    }
+    dispatch({
+      type: ORDER_LIST_MY_FAIL,
+      payload: message,
     })
   }
 }
